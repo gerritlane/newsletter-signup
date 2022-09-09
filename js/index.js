b30c23b45c63342780a31163e697a31e9431d936
Gerrit Lane <lane.gerrit@gmail.com>
2022-09-08 20:31:48 -0700

    Change inputs on form and create public folder

--- js/index.js
+++ js/index.js
@@ -4,8 +4,21 @@ const https = require('https');
 const xss = require('xss');
 const app = express();
 
-
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+// app.get('/css/styles.css', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../css/styles.css'));
+// });
+
+// app.get('/images/logo.svg', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../images/logo.svg'));
+// });
+
 
 app.listen(3000, function(){
 	console.log('Server started on port 3000');
