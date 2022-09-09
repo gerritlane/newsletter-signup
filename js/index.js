a4cb870c2da8abd6b445b1896e8c5e832060a712
Gerrit Lane <lane.gerrit@gmail.com>
2022-09-08 20:50:12 -0700

    Add form post route

--- js/index.js
+++ js/index.js
@@ -11,14 +11,10 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-// app.get('/css/styles.css', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../css/styles.css'));
-// });
-
-// app.get('/images/logo.svg', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../images/logo.svg'));
-// });
-
+app.post('/index.html', (req, res) => {
+	console.log(req.body);
+	res.send('Thank You!');
+});
 
 app.listen(3000, function(){
 	console.log('Server started on port 3000');
