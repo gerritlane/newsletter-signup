c5a52e4597fa155184a66dd7fc7f0200e119d673
Gerrit Lane <lane.gerrit@gmail.com>
2022-09-12 16:01:10 -0700

    Add mailchimp api call to post method

--- js/index.js
+++ js/index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const path = require('path');
 const https = require('https');
 const xss = require('xss');
+const mailchimp = require('@mailchimp/mailchimp_marketing');
 const app = express();
+
+// Configure environment
 require('dotenv').config();
 const api_key = process.env.MAILCHIMP_API_KEY;
+const mc_server = process.env.MAILCHIMP_SERVER;
+const audience_id = process.env.MAILCHIMP_AUDIENCE_ID;
+mailchimp.setConfig({
+  apiKey: api_key,
+  server: mc_server,
+});
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +24,25 @@ app.get('/', (req, res) => {
 
 app.post('/index.html', (req, res) => {
 	console.log(req.body);
-	res.send('Thank You!');
+	const firstName = xss(req.body.firstName);
+	const lastName = xss(req.body.lastName);
+	const email = xss(req.body.email);
+	const addSubscriber = async() =>	{
+		const response = await mailchimp.lists.batchListMembers(audience_id, {
+			members: [{
+				email_address: email,
+				status: "subscribed",
+				merge_fields: {
+					FNAME: firstName,
+					LNAME: lastName
+				}
+			}],
+		});
+		console.log(response);
+	};
+	addSubscriber();
+
+	res.send('Success!');
 });
 
 app.listen(3000, function(){
